9ad59a5687c0d93b941af0591c3d24c57786f80f
Rishita Dixit <rishitadixit02@gmail.com>
2026-08-16 23:17:58 +0530

    Time: 6 ms (43.65%) | Memory: 131.1 MB (92.89%) - LeetSync

--- 2156-stone-game-ix/stone-game-ix.cpp
+++ 2156-stone-game-ix/stone-game-ix.cpp
@@ -1,23 +1,23 @@
 class Solution {
 public:
     bool stoneGameIX(vector<int>& stones) {
+        int count0=0;
         int count1=0;
         int count2=0;
-        int count3=0;
         for(int i=0;i<stones.size();i++){
             if(stones[i]%3==0){
-                count1++;
+                count0++;
             }
             else if(stones[i]%3==1){
-                count2++;
+                count1++;
             }
             else{
-                count3++;
+                count2++;
             }
         }
-            if(count1%2==0){
-                return count2>=1 && count3>=1;
+            if(count0%2==0){
+                return count1>=1 && count2>=1;
             }
-        return abs(count2-count3)>2;
+        return abs(count1-count2)>2;
     }
 };
